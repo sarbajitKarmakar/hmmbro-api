cc326e86866697d47dba9daf3008e66708b2b9ae
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2025-10-14 19:29:04 +0530

    auth function

--- services/auth.js
+++ services/auth.js
@@ -1,1 +1,27 @@
 import jwt from 'jsonwebtoken';
+
+const secret = process.env.JWT_SECRET || 'your_jwt_secret';
+
+const generateToken = (user) => {
+    const payload = {
+        email: user.email,
+        username: user.username,
+        phone: user.phone,
+        pic: user.pic,
+        role: user.role,
+        active: user.active,
+    }
+
+    return jwt.sign(payload, secret);
+
+}
+
+const verifyToken = (token) => {
+    try {
+        return jwt.verify(token, secret);
+    } catch (err) {
+        return null;
+    }
+
+}
+export { generateToken,verifyToken };
