c3b62f88f27e38cc899af6b4d78bfc5a4b62f7a8
Sarbajit Karmakar <sarbajitkarmakar26@gmail.com>
2025-10-15 13:11:20 +0530

    feat: add password hashing and verification functions using bcrypt

---
+++ services/hashPass.js
@@ -0,0 +1,25 @@
+import bcrypt from "bcrypt";
+
+const saltRounds = 10;
+
+// Function to hash a password
+const hashPassword = async (plainPassword) => {
+  try {
+    const hashedPassword = await bcrypt.hash(plainPassword, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.error("Error hashing password:", error);
+  }
+};
+
+// Function to verify a password
+verifyPassword = async (plainPassword, hashedPassword) => {
+  try {
+    const match = await bcrypt.compare(plainPassword, hashedPassword);
+    return match;
+  } catch (error) {
+    console.error("Error verifying password:", error);
+  }
+};
+
+export { hashPassword, verifyPassword };
